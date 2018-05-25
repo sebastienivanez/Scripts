4048166f3627b80193bda81ec52346002c1b734a
Sebastien Ivanez <s.ivanez@televic.com>
2018-05-25 14:36:55 +0200

    Adding test repo to run quickly c debug. Also adding stuff in todo list.

---
+++ test/main.c
@@ -0,0 +1,16 @@
+/**
+ * Autor	Sebastien Ivanez (sebastien@ivanez.com)
+ * Date		xx/xx/xxxx
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, const char *argv[])
+{
+	return 0;
+}
