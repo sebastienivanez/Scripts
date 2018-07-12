a17f91bb5a38f48995ef90e3f9660648ce9022a2
Sebastien Ivanez <s.ivanez@televic.com>
2018-07-12 12:13:48 +0200

    Adapt file to doxygen syntaxe and add extra infos on include libraries

--- test/main.c
+++ test/main.c
@@ -1,16 +1,23 @@
-/**
- * Autor	Sebastien Ivanez (sebastien@ivanez.com)
- * Date		xx/xx/xxxx
+/*! \file
+ * Test purposes
  */
+ 
+/* INCLUDES */
+#include <stdio.h> // To call printf()
+//#include <stdlib.h> // To call malloc()
+//#include <unistd.h> // To call read()
+//#include <fcntl.h> // To call open()
+//#include <stdint.h> // To use uint8_t
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <stdbool.h>
+/* MACROS */
 
-int main(int argc, const char *argv[])
+/*!
+ * \details Entry function to project.
+ * \author  Sebastien Ivanez
+ * \date    12/07/2018
+ */
+int main(int argc, char *argv[])
 {
+	printf("Hello Seb !\n");
 	return 0;
 }
