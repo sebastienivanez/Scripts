bb802df504ee16e1957ff381fb14b99f6637ad7a
Sebastien Ivanez <s.ivanez@televic.com>
2018-07-13 19:07:03 +0200

    Adding includes with comments in test repository.

--- test/main.c
+++ test/main.c
@@ -4,10 +4,14 @@
  
 /* INCLUDES */
 #include <stdio.h> // To call printf()
-//#include <stdlib.h> // To call malloc()
+//#include <stdlib.h> // To call malloc()/sleep()
 //#include <unistd.h> // To call read()
 //#include <fcntl.h> // To call open()
 //#include <stdint.h> // To use uint8_t
+//#include <cstdlib> // To call atoi()
+//#include <sstream> // To call stringstream
+//#include <iomanip> // To call setw()
+//#include <iostream> // To call cout/cin
 
 /* MACROS */
 
